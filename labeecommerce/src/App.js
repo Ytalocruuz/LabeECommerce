6c9fa8ca9f5862e8b4368da5c753e4193c9300ac
nomytalo <email>
2022-02-09 10:01:16 -0300

    concluido

--- labeecommerce/src/App.js
+++ labeecommerce/src/App.js
@@ -1,103 +1,141 @@
-import React from 'react'
-import { ContainerPrincipal, TituloPrincipal } from './AppStyled';
-import { Carinho } from './Components/Carinho/Carinho';
-import Filtros from './Components/Filtros/Filtros';
-import { Produtos } from './Components/Produtos/Produtos';
-import { products } from './product';
+import React from 'react';
+import { Filters } from './components/Filters/Filters';
+import { Products } from './components/Products/Products';
+import { ShoppingCart } from './components/ShoppingCart/ShoppingCart';
+import styled from 'styled-components';
 
+const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  padding: 16px;
+  gap: 8px;
+`;
+
+const products = [
+  {
+    id: 1,
+    name: 'Produto legal',
+    price: 123,
+    photo: 'https://picsum.photos/200/200?a=1'
+  },
+  {
+    id: 2,
+    name: 'Produto 2',
+    price: 200,
+    photo: 'https://picsum.photos/200/200?a=2'
+  },
+  {
+    id: 3,
+    name: 'Produto 3',
+    price: 30,
+    photo: 'https://picsum.photos/200/200?a=3'
+  },
+  {
+    id: 4,
+    name: 'Produto 4',
+    price: 10,
+    photo: 'https://picsum.photos/200/200?a=4'
+  }
+]
 
 class App extends React.Component {
-
   state = {
-    filtroMinimo: 100,
-    filtroMaximo: 1000,
-    filtroBuscarPorNome: "",
+    minFilter: 100,
+    maxFilter: 1000,
+    nameFilter: 'Produto',
     productsInCart: [
       {
-        id: 2,
-        nome: "Produto legal 2",
-        price: 233,
-        phone: 'https://picsum.photos/200/200',
+        id: 4,
+        name: 'Produto 4',
+        price: 10,
+        photo: 'https://picsum.photos/200/200?a=4',
         quantity: 1
       },
       {
-        id: 2,
-        nome: "Produto legal 2",
-        price: 233,
-        phone: 'https://picsum.photos/200/200',
-        quantity: 1
-      },
-      {
-        id: 2,
-        nome: "Produto legal 2",
-        price: 233,
-        phone: 'https://picsum.photos/200/200',
-        quantity: 1
+        id: 3,
+        name: 'Produto 3',
+        price: 30,
+        photo: 'https://picsum.photos/200/200?a=3',
+        quantity: 2
       }
     ]
   }
 
-  manipularValorDoFiltroMinimo = (event) => {
-    // console.log(event.target.value)
-    this.setState({
-      filtroMinimo: event.target.value
-    })
+  onChangeMinFilter = (event) => {
+    this.setState({minFilter: event.target.value})
   }
 
-  manipularValorDoFiltroMaximo = (event) => {
-    // console.log(event.target.value)
-    this.setState({
-      filtroMaximo: event.target.value
-    })
+  onChangeMaxFilter = (event) => {
+    this.setState({maxFilter: event.target.value})
   }
-  manipularValorDoFiltroBuscarPorNome = (event) => {
-    // console.log(event.target.value)
-    this.setState({
-      filtroBuscarPorNome: event.target.value
-    })
+
+  onChangeNameFilter = (event) => {
+    this.setState({nameFilter: event.target.value})
   }
+
+  onAddProductToCart = (productId) => {
+    const productInCart = this.state.productsInCart.find(product => productId === product.id)
+
+    if(productInCart) {
+      const newProductsInCart = this.state.productsInCart.map(product => {
+        if(productId === product.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1
+          }
+        }
+
+        return product
+      })
+
+      this.setState({productsInCart: newProductsInCart})
+    } else {
+      const productToAdd = products.find(product => productId === product.id)
+
+      const newProductsInCart = [...this.state.productsInCart, {...productToAdd, quantity: 1}]
+
+      this.setState({productsInCart: newProductsInCart})
+    }
+  }
+
+  onRemoveProductFromCart = (productId) => {
+    const newProductsInCart = this.state.productsInCart.map((product) => {
+      if(product.id === productId) {
+        return {
+          ...product,
+          quantity: product.quantity - 1
+        }
+      }
+      return product
+    }).filter((product) => product.quantity > 0)
+
+    this.setState({productsInCart: newProductsInCart})
+  }
+
   render() {
-
-    const pacoteFiltradosMinimo = products.filter(produto => {
-      if (this.state.filtroMinimo) {
-        return produto.price >= this.state.filtroMinimo
-      }
-    })
-
-    const pacoteFiltradosMaximo = products.filter(produto => {
-      if (this.state.filtroMaximo) {
-        return produto.price <= this.state.filtroMaximo
-      } else {
-        return produto
-      }
-    })
-
-    console.log(pacoteFiltradosMaximo)
-
-    return <div>
-
-      <TituloPrincipal><h1>LabECommerce</h1></TituloPrincipal>
-
-      <ContainerPrincipal>
-        <Filtros
-          minimo={this.state.filtroMinimo}
-          maximo={this.state.filtroMaximo}
-          buscaPorNome={this.state.filtroBuscarPorNome}
-
-          onChangeMinimo={this.manipularValorDoFiltroMinimo}
-          onChangeMaximo={this.manipularValorDoFiltroMaximo}
-          onChangeBuscarPorNome={this.manipularValorDoFiltroBuscarPorNome}
+    return (
+      <AppContainer>
+        <Filters
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+          onChangeMinFilter={this.onChangeMinFilter}            
+          onChangeMaxFilter={this.onChangeMaxFilter}            
+          onChangeNameFilter={this.onChangeNameFilter}                  
         />
-
-        <Produtos products={products}
-          minimo={this.state.filtroMinimo}
-          maximo={this.state.filtroMaximo}
-          buscaPorNome={this.state.filtroBuscarPorNome} />
-        <Carinho 
-        productsInCart={this.state.productsInCart}/>
-      </ContainerPrincipal>
-
-    </div>
+        <Products 
+          products={products}
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+          onAddProductToCart={this.onAddProductToCart}
+        />
+        <ShoppingCart
+          productsInCart={this.state.productsInCart}
+          onRemoveProductFromCart={this.onRemoveProductFromCart}
+        />
+      </AppContainer>
+    );
   }
 }
 
